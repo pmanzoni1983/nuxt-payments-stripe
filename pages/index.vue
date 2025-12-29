e996467d3a72ae3ec4ea9ce967671853579a57f3
Pablo Manzoni <pmanzoni@gmail.com>
2025-12-28 20:23:03 -0600

    body sent on button click event

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,59 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 // import { useNuxtApp } from '#app'
 
-const pending = ref(false)
-const payWithStripe = async () => {
-	pending.value = true
+type OrderBody = {
+  product_name: string;
+  product_description?: string;
+  image?: string;
+  images?: string[];
+  quantity?: number;
+  price: number;
+};
 
-	const response = await $fetch<{ url: string }>('/api/create-order', {
-		method: 'POST',
-		body: {
-			product_name: 'Nuxt Course',
-			product_description: 'Learn how to integrate Stripe payments with Nuxt 3',
-			image: 'https://nuxt.com/logo.png',
-			quantity: 3,
-			price: 50,
-		},
-	})
+// Default order payload used by the button
+const order: OrderBody = {
+  product_name: "Nuxt Course",
+  product_description:"A Nuxt course teaches you to build modern web apps with Nuxt, a powerful, open-source framework on top of Vue.js, covering features like file-based routing, server-side rendering (SSR), static site generation, API routes, auto-imports, and performance optimization for faster, SEO-friendly full-stack applications. Courses guide you from project setup (using Vite) to deploying production-ready sites, simplifying complex tasks for both beginners and experienced developers. ",
+  image: "https://nuxt.com/logo.png",
+  quantity: 2,
+  price: 50,
+};
 
-	pending.value = false
+const pending = ref(false);
+const payWithStripe = async (body: OrderBody) => {
+  pending.value = true;
 
-	if (response.url) {
-		window.location.href = response.url
-	} else {
-		alert('Something went wrong')
-	}
-}
+  const response = await $fetch<{ url: string }>("/api/create-order", {
+    method: "POST",
+    body,
+  });
+
+  pending.value = false;
+
+  if (response.url) {
+    window.location.href = response.url;
+  } else {
+    alert("Something went wrong");
+  }
+};
 </script>
 
 <template>
-	<UContainer class="flex flex-col gap-6 items-center py-24">
-		<UCard class="max-w-sm w-full">
-			<template #header>
-				<h1 class="font-bold text-xl text-center">Payments in Nuxt</h1>
-			</template>
-			<UButton
-				@click="payWithStripe"
-				:loading="pending"
-				block
-				size="xl"
-				icon="i-mdi-credit-card"
-			>
-				Pay with Stripe
-			</UButton>
-		</UCard>
-	</UContainer>
+  <UContainer class="flex flex-col gap-6 items-center py-24">
+    <UCard class="max-w-sm w-full">
+      <template #header>
+        <h1 class="font-bold text-xl text-center">Payments in Nuxt</h1>
+      </template>
+      <UButton
+        @click="payWithStripe(order)"
+        :loading="pending"
+        block
+        size="xl"
+        icon="i-mdi-credit-card"
+      >
+        Pay with Stripe
+      </UButton>
+    </UCard>
+  </UContainer>
 </template>
